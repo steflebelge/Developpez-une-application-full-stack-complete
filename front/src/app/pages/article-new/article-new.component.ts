c4ea59b9c95cd1ad0503c593755306bb981bbb19
stefan.gualandi <stefan.gualandi@aam.fr>
2026-01-16 17:37:38 +0100

    feat (front) : articles components for list and create

--- front/src/app/pages/article-new/article-new.component.ts
+++ front/src/app/pages/article-new/article-new.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Theme} from "../../models/theme.model";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ApiService} from "../../services/api.service";
+import {Location} from "@angular/common";
 
 @Component({
   selector: 'app-article-new',
@@ -6,10 +10,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article-new.component.scss']
 })
 export class ArticleNewComponent implements OnInit {
+  form!: FormGroup;
+  themes: Theme[] = [];
+  error: string = '';
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private location: Location, private apiService: ApiService) {}
 
   ngOnInit(): void {
+    // 1️⃣ Créer le formulaire
+    this.form = this.fb.group({
+      themeId: [null, Validators.required],
+      title: ['', [Validators.required, Validators.maxLength(100)]],
+      content: ['', [Validators.required, Validators.minLength(10)]],
+    });
+
+    this.loadThemes();
   }
 
+  loadThemes(): void {
+    //récupérer les thèmes
+    this.apiService.get<Theme[]>('themes/getAll').subscribe({
+      next: (res) => this.themes = res,
+      error: () => this.error = 'Erreur lors de la récupération des themes'
+    });
+  }
+
+  submit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const payload = this.form.value;
+    this.apiService.post('articles/create', payload).subscribe({
+      next: () => {
+        console.log('Post créé !');
+        this.form.reset();
+      },
+      error: (err) => {
+        this.error = err.error?.message || 'Erreur lors de la création du post';
+      }
+    });
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
